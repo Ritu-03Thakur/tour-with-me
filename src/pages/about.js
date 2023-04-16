8238f39121f8989b66ac2a0e240bfc30a8c82466
Ritu <ritutec001@gmail.com>
2023-04-17 01:21:48 +0530

    ..

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-      
+        <h1 className= "text-white-500 dark:text-white-400 "> About Tour With me </h1>
 <p class="text-gray-500 dark:text-gray-400">Due to its central geographic location in Southern Europe, <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline" data-popover-target="popover-image">Italy</a> has historically been home to myriad peoples and cultures. In addition to the various ancient peoples dispersed throughout what is now modern-day Italy, the most predominant being the Indo-European Italic peoples who gave the peninsula its name, beginning from the classical era, Phoenicians and Carthaginians founded colonies mostly in insular Italy</p>
 <div data-popover id="popover-image" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-96 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
     <div class="grid grid-cols-5">
